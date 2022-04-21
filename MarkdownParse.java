d4e3b7cc27b21ff237c7963b5a9610c7d95b1549
Elaine Chao <elaine.iy@outlook.com>
2022-04-21 08:21:16 -0700

    add tester

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -14,7 +14,7 @@ public class MarkdownParse {
         while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
-            int openParen = markdown.indexOf("(", closeBracket);
+            int openParen = markdown.indexOf("(", currentIndex);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
